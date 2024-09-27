07454c772f6c8b19fe5c1381a50931864fcb9376
MrCherry89 <abgarnati@yandex.ru>
2024-09-27 17:29:22 +0400

    ok

--- js/script.js
+++ js/script.js
@@ -413,5 +413,23 @@ $(document).ready(function () {
     else sticky.removeClass("show");
   });
 
+  $('input[name="dates"]').daterangepicker();
+
+  $(function () {
+    $('input[name="daterange"]').daterangepicker(
+      {
+        opens: "left",
+      },
+      function (start, end, label) {
+        console.log(
+          "A new date selection was made: " +
+            start.format("YYYY-MM-DD") +
+            " to " +
+            end.format("YYYY-MM-DD")
+        );
+      }
+    );
+  });
+
   AOS.init();
 });
